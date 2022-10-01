f27310aad24095ce4a9832f0ba0a3a57d23d5533
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2022-10-01 16:52:42 +0500

    Add files via upload

---
+++ 1st practice/hello_world.cpp
@@ -0,0 +1,130 @@
+// #include <iostream>
+
+// int main() {
+//     std::cout << "Hello Pakistan!";
+//     return 0;
+// }
+
+/*xxxxxxxxxx*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// INT    
+    //  int a;   //declaration//
+    //  a=12;   //initialisation//
+
+    //  with std
+    //  std::cout<<"size of int "<<sizeof(a)std::<<endl;
+
+    //  float b;
+    //  std::cout<<"size of float "<<sizeof(b)std::<<endl;
+     
+    //  char c;
+    //  std::cout<<"size of char "<<sizeof(c)std::<<endl;
+
+    //  bool d;
+    //  std::cout<<"size of bool "<<sizeof(d)std::<<endl;
+
+    //  without std
+    //  cout<<"size of int "<<sizeof(a)<<endl;
+
+    //  float b;
+    //  cout<<"size of float "<<sizeof(b)<<endl;
+     
+    //  char c;
+    //  cout<<"size of char "<<sizeof(c)<<endl;
+
+    //  bool d;
+    //  cout<<"size of bool "<<sizeof(d)<<endl;
+     
+    //*  modifier *//
+    // short int si;
+    // long int li;
+    // std::cout<<"size of shortint "<<sizeof(si)<<std::endl;
+    // std::cout<<"size of longint "<<sizeof(li)<<std::endl;
+
+    
+
+    //* Input/Output:-*//
+    // int amount1;
+    // cin>>amount1;
+
+    // int amount2;
+    // cin>>amount2;
+
+    // int sum = amount1 + amount2;
+    // cout<<"sum\n";
+    // cout<<sum;
+
+
+
+    //* IF-Else Statement:- *//   /*here find max  number*/
+//     int a,b,c;
+//     cin>>a>>b>>c;
+
+//     if (a>b){
+//         if (a>c){
+//             cout<<a<<endl;
+//         }
+//         else{
+//             cout<<c<<endl;
+//         }
+//     }
+// else{
+//     if (b>c){
+//         cout<<b<<endl;
+//     }
+//     else{
+//         cout<<c<<endl;
+//     }
+// }
+
+                           /*if else but even odd numbers check */
+//     int n;
+//     cin>>n;
+
+//     if (n%2==0){
+//         cout<<"Even"<<endl;
+//     }
+//     else{
+//         cout<<"Odd"<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+/*Loops In C++:-*/
+
+/*For Loop  in C++*/
+// int n;
+// cin>>n;
+//  int b = 0;
+//  for(int a = 1; a <= n; a++){
+//      b = a + b; 
+//  }
+//  cout<<b<<endl;
+//  return 0;
+//  }
+
+
+// int b = 0;
+//  for(int a = 1; a <= 5; a++){
+//      b = a + b; 
+//  }
+//  cout<<b<<endl;
+//  return 0;
+// }
+
+
+// int a;
+// cin>>a;
+// int b = 1;
+// for(a = 0; a <= 10; a++){
+//     b = a + b;
+// }
+// cout<<b<<endl;
+// return 0;
+// }
